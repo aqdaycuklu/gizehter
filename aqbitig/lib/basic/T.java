a97dea65ea623d1f03ad2ac87138fff919f61dad
aqdaycuklu <aqdaycuklu@aqbitig>
2020-04-08 21:55:04 +0200

    MODIFY: tools

--- aqbitig/lib/basic/T.java
+++ aqbitig/lib/basic/T.java
@@ -14,17 +14,15 @@ public class T {
     }
 
     public static void o(boolean b) {
-        for (int i = 0; i < Thread.currentThread().getStackTrace().length; i++) {
-            String c = Thread.currentThread().getStackTrace()[i].getClassName();
-            String m = Thread.currentThread().getStackTrace()[i].getMethodName();
-            System.out.println(c + "." + m + "()");
-        }
+        String c = Thread.currentThread().getStackTrace()[2].getClassName();
+        String m = Thread.currentThread().getStackTrace()[2].getMethodName();
+        System.out.println(c + "." + m + "() | " + (b ? "true" : "false"));
     }
 
     public static void o(int i) {
-        String c = Thread.currentThread().getStackTrace()[i].getClassName();
-        String m = Thread.currentThread().getStackTrace()[i].getMethodName();
-        System.out.println(c + "." + m + "()");
+        String c = Thread.currentThread().getStackTrace()[2].getClassName();
+        String m = Thread.currentThread().getStackTrace()[2].getMethodName();
+        System.out.println(c + "." + m + "() | " + i);
     }
 
     public static void o(Object o) {
